bb302a517395267db27e2c547513ebb1e1df1eb8
regista766 <33421982+systemweb766@users.noreply.github.com>
2018-02-10 04:41:05 +0900

    merge commit 201802100441

--- test.php
+++ test.php
@@ -2,35 +2,64 @@
 //error
 $err = "";
 
-//ディレクトリパス
-$dir = "/data/";
-
-$check_dirs[] = $dir;
-$file_paths = [];
-while($check_dirs)
+//入力チェック
+if($_POST['year'] == "")
 {
-  $dir_paths = $check_dirs[0];
-    // if(is_dir($dir_paths) && $handle = opendir($dir_paths))
-    // {
-    //   ver_dump($handle);
-    //   exit;
-    //   // while(($file = readdir($handle)) !== false)
-    //   // {
-    //   //   if(in_array($file, [".", ".."]))
-    //   // }
-    // }
-  //入力チェック
-  if($_POST['year'] == "")
-  {
-      $err .= "select err!";
-  }
+  $err .= "select err!";
+}
+//サニタイズ&POST取得
+$post['year'] = $_POST['year'];
+//ディレクトリパス
+$dir = "./data/";
 
-  //サニタイズ&POST取得
-  $post['year'] = $_POST['year'];
+var_dump(fileAndPathSearcher($dir));
+
 
   //ファイル名の西暦取得
-  // $data['year'] =
+  $data['year'] = substr($file, 0, 4);
+  if($data['year'] === $post['year']){
+    echo $data['year'];
+  }
 
+
+
+
+
+
+
+// funcitons
+
+function fileAndPathSearcher($dir){
+  //ディレクトリパス
+  $directory = $dir;
+  $check_dirs = [$directory];
+  $data['file'][] = "";
+  $data['path'][] = "";
+
+  while($check_dirs)
+  {
+    $dir_paths = $check_dirs[0];
+      if(is_dir($dir_paths) && $handle = opendir($dir_paths))
+      {
+        while(($file = readdir($handle)) !== false)
+        {
+          if(in_array($file, [".", ".."]) !== false)
+          {
+            continue;
+          }
+          //ファイルパス作成
+          $path = rtrim($dir_paths , "/") . "/" . $file;
+          if(filetype($path) === "dir") {
+            $check_dirs[] = $path;
+          }
+          else {
+            $data['file'][] = $file;
+            $data['path'][] = $path;
+          }
+        }
+      }
+  array_shift($check_dirs);
+  }
+  return $data;
 }
-
  ?>
